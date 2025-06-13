4b006d4707b54caa3d561883b2d86e9aad674679
AdityaLF <adityaalf9@gmail.com>
2025-06-13 20:02:08 +0800

    Update quick-links.tsx

--- components/quick-links.tsx
+++ components/quick-links.tsx
@@ -31,7 +31,7 @@ const links = [
   },
   {
     name: "More",
-    url: "https://linktr.ee/adityadf",
+    url: "https://linktr.ee/adityaf",
     color: "hover:bg-purple-500/20 hover:border-purple-500/50",
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link-icon lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>
